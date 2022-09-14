594d44167ba065676364279c8e622bd2464f93f8
Peter Fačko <pfacko1@gmail.com>
2022-09-14 17:10:12 +0200

    report update

--- report/perf.sh
+++ report/perf.sh
@@ -12,10 +12,10 @@ inputs_dir="$2"
 
 parse_output()
 {
-    sed "s/$/,$option_cdcl,$option_adjacency,$option_clause,$option_decision,$is_sat/"
+    sed "s/$/,$option_cdcl,$option_adjacency,$option_clause,$option_decision,$option_virtual,$is_sat/"
 }
 
-echo "value,?1,counter,?2,?3,ratio,?5,cdcl,adjacency,clause,decision,sat"
+echo "value,?1,counter,?2,?3,ratio,?5,cdcl,adjacency,clause,decision,virtual,sat"
 
 for option_cdcl in "true" "false"
 do
@@ -23,9 +23,11 @@ for option_adjacency in "list" "set" "set_unordered"
 do
 for option_clause in "basic" "counting" "watched_literals"
 do
-for option_decision in "trivial" "random" "JW_static" "VSIDS"
+for option_decision in "trivial" "deterministic" "random" "JW_static" "VSIDS"
 do
-    options="-random 1 -cdcl $option_cdcl -adjacency $option_adjacency -clause $option_clause -decision $option_decision -output csv"
+for option_virtual in "true" "false"
+do
+    options="-random 1 -cdcl $option_cdcl -adjacency $option_adjacency -clause $option_clause -decision $option_decision -virtual $option_virtual -output csv"
 
     for is_sat in "true" "false"
     do
@@ -37,10 +39,11 @@ do
         fi
         for i in "10" "11" "12" "13" "14"
         do
-            perf stat -x, -e instructions,L1-dcache-loads,L1-dcache-load-misses,branches,branch-misses,page-faults \
+            perf stat -x, -e cycles,instructions,page-faults,L1-dcache-loads,L1-dcache-load-misses,branches,branch-misses \
             "$solver" $options "$inputs_dir/${file_prefix}100-0$i.cnf" 2>&1 >/dev/null | parse_output
         done
     done
+done
 done
 done
 done
